ef0a8504e21b27fa8784dae512ed3131cff0ea5a
kim-hyunjin <hyunjin1612@gmail.com>
2021-10-22 22:14:25 +0900

    Spring Cloud Gateway - Load Balancer

--- second-service/src/main/java/com/example/secondservice/SecondServiceController.java
+++ second-service/src/main/java/com/example/secondservice/SecondServiceController.java
@@ -1,13 +1,22 @@
 package com.example.secondservice;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("/second-service")
+@RequiredArgsConstructor
+@Slf4j
 public class SecondServiceController {
+
+    private final Environment env;
 
     @GetMapping("/welcome")
     public String welcome() {
@@ -21,7 +30,7 @@ public class SecondServiceController {
     }
 
     @GetMapping("/check")
-    public String check() {
-        return "Hi, there. This is a Message from Second Service";
+    public String check(HttpServletRequest request) {
+        return String.format("Hi, there. This is a Message from Second Service on PORT = %s", env.getProperty("local.server.port") );
     }
 }
