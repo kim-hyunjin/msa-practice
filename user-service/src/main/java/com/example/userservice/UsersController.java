1dcb6fd98b4456abf49ffd16194acb0d35056227
kim-hyunjin <hyunjin1612@gmail.com>
2021-10-27 13:21:52 +0900

    Users MicroService와 Spring Cloud Gateway 연동

--- user-service/src/main/java/com/example/userservice/UsersController.java
+++ user-service/src/main/java/com/example/userservice/UsersController.java
@@ -7,17 +7,19 @@ import com.example.userservice.vo.ResponseUser;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/")
+@RequestMapping("/user-service")
 public class UsersController {
 
     private final ModelMapper modelMapper;
     private final UserService userService;
+    private final Environment env;
 
     @Value("${greeting.message}")
     private String message;
@@ -33,7 +35,7 @@ public class UsersController {
     }
 
     @GetMapping("/health-check")
-    public String status() { return "It's working"; }
+    public String status() { return String.format("It's working in User Service on PORT %s", env.getProperty("local.server.port")); }
 
     @GetMapping("/welcome")
     public String welcome() {
