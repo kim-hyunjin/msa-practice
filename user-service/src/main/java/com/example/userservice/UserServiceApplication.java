7776c37d546aa37c8fc09393d3fb85b97de144f4
kim-hyunjin <hyunjin1612@gmail.com>
2021-10-29 21:55:36 +0900

    UPDATE: api gateway order micro service route info

--- user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -2,14 +2,12 @@ package com.example.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import javax.persistence.EntityListeners;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 @EnableJpaAuditing
 public class UserServiceApplication {
 
