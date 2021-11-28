6439178379189e707221df03b30ca1c614923e34
kim-hyunjin <hyunjin1612@gmail.com>
2021-11-28 23:46:23 +0900

    user-service에서 authorization이 필요한 url 수정

--- user-service/src/main/java/com/example/userservice/security/WebSecurity.java
+++ user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -27,7 +27,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
 //        http.authorizeRequests().antMatchers("/users/**").permitAll();
-        http.authorizeRequests().antMatchers("/**")
+        http.authorizeRequests().antMatchers("/users/**")
                 .hasIpAddress("127.0.0.1")
                 .and()
                 .addFilter(getAuthenticationFilter());
