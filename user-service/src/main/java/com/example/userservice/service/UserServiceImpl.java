33dc16dc3cdb5640e13753afaaf074265d1e59a9
kim-hyunjin <hyunjin1612@gmail.com>
2021-12-09 21:23:04 +0900

    Resilience4J를 활용하여 user micro service에 CircuitBreaker 적용

--- user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -5,20 +5,17 @@ import com.example.userservice.dto.UserDto;
 import com.example.userservice.jpa.UserEntity;
 import com.example.userservice.jpa.UserRepository;
 import com.example.userservice.vo.ResponseOrder;
-import feign.FeignException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
-import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreaker;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.core.env.Environment;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -41,6 +38,8 @@ public class UserServiceImpl implements UserService {
 
     private final OrderServiceClient orderServiceClient;
 
+    private final CircuitBreakerFactory circuitBreakerFactory;
+
     @Override
     public UserDto createUser(UserDto userDto) {
         userDto.setUserId(UUID.randomUUID().toString());
@@ -61,22 +60,36 @@ public class UserServiceImpl implements UserService {
 
         UserDto userDto = modelMapper.map(userEntity, UserDto.class);
 
-        // order 정보 - using rest template
-//        String orderUrl = String.format(env.getProperty("order_service.url"), userId);
-//        ResponseEntity<List<ResponseOrder>> orderListResponse =
-//                restTemplate.exchange(orderUrl, HttpMethod.GET, null,
-//                        new ParameterizedTypeReference<List<ResponseOrder>>() {});
-//        userDto.setOrders(orderListResponse.getBody());
+        /* order 정보 - using rest template
 
-        // order 정보 - using FeignClient
-//        List<ResponseOrder> orderList = null;
-//        try {
-//            orderList = orderServiceClient.getOrders(userId);
-//        } catch (FeignException ex) {
-//            log.error(ex.getMessage());
-//        }
-        // feign.codec.ErrorDecoder 사용
+        String orderUrl = String.format(env.getProperty("order_service.url"), userId);
+        ResponseEntity<List<ResponseOrder>> orderListResponse =
+                restTemplate.exchange(orderUrl, HttpMethod.GET, null,
+                        new ParameterizedTypeReference<List<ResponseOrder>>() {});
+        userDto.setOrders(orderListResponse.getBody());
+
+        */
+
+        /* order 정보 - using FeignClient
+
+        List<ResponseOrder> orderList = null;
+        try {
+            orderList = orderServiceClient.getOrders(userId);
+        } catch (FeignException ex) {
+            log.error(ex.getMessage());
+        }
+
+        */
+
+        /* feign.codec.ErrorDecoder 사용
+
         List<ResponseOrder> orderList = orderServiceClient.getOrders(userId);
+
+         */
+        CircuitBreaker circuitBreaker = circuitBreakerFactory.create("orderlist-circuitbreaker");
+        List<ResponseOrder> orderList =
+                circuitBreaker.run(() -> orderServiceClient.getOrders(userId), throwable -> new ArrayList<>());
+
         userDto.setOrders(orderList);
 
         return userDto;
